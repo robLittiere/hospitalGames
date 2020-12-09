8f3fa57987afcf3915e426fd692a63a1f60bb85f
robLittiere <robinl.95@orange.fr>
2020-12-09 14:50:33 +0100

    Commit Menu

---
+++ src/com/company/playlistLaunch.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class playlistLaunch {
+
+    String playlistMenu = "p";
+
+
+    public static void playlistLaunch(){
+        System.out.println("Hi this is where you manage and listen to your playlist");
+        List<Playlist> listePlaylist = new ArrayList<>();
+
+
+
+
+    }
+}

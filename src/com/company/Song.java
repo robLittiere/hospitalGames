21d482935dd44e54d473114a9e51b292a8663e94
Léo <leo.gregori95@gmail.com>
2020-12-09 15:29:25 +0100

    Update to take playlistLaunch

--- src/com/company/Song.java
+++ src/com/company/Song.java
@@ -27,30 +27,4 @@ public class Song {
         return duration;
     }
 
-
-    public static void isInThePlaylist(){
-        /*for (int i = 0; i < playlistJul.size(); i++) {
-            if (playlistJul.get(i).getSongName().equals(songNameAsked) && playlistJul.get(i).getAuthor().equals(songAuthorAsked)) {
-                indexCurentSong = playlistJul.indexOf(songNameAsked);
-                System.out.println("Your now listening " + playlistJul.get(i).getSongName() + " de " + playlistJul.get(i).getAuthor());
-                break;
-            } else if (!playlistJul.get(i).getSongName().equals(songNameAsked)) {
-                System.out.println("This song is not in your playlist");
-            } else if (!playlistJul.get(i).getAuthor().equals(songAuthorAsked)){
-                System.out.println("We don't have this song with this author");
-            }
-        }
-
-        System.out.println("");*/
-    }
-    public void listenSong(){
-        System.out.println("What song do you want to listen ?");
-        Scanner songAsked;
-        songAsked  = new Scanner(System.in);
-        this.songNameAsked = songAsked.next();
-        System.out.println("Who is the author?");
-        this.songAuthorAsked = songAsked.next();
-        isInThePlaylist();
-        System.out.println("");
-    }
 }
